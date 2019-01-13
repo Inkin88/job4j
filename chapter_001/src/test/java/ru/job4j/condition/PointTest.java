44617ef194280fe75676ec0b03fea7cfd2142e07
Inkin88 <inkin_88@mail.ru>
2019-01-13 22:18:57 +0300

    3.4. Расстояние между точками в системе координат[#100349]

---
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+public class PointTest  {
+    @Test
+    public void distanceFromPointAToB() {
+        Point a = new Point (0, 1);
+        Point b = new Point(2, 5);
+        double result = a.distanceTo(b);
+        double expected = 4.47213595499958;
+        assertThat(result, closeTo(expected, 0.1));
+    }
+}
