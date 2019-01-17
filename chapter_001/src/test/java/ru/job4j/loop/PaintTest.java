172782f498b9999c18d0ed148ad340858ee639a0
Inkin88 <inkin_88@mail.ru>
2019-01-17 20:54:37 +0300

    5.4. Построить пирамиду в псевдографике.[#100331]

---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+
+import java.util.StringJoiner;
+
+import static org.junit.Assert.*;
+
+public class PaintTest {
+    @Test
+    public void whenPeramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst, is(new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("^   ")
+                .add("^^  ")
+                .add("^^^ ")
+                .add("^^^^")
+                .toString()
+        ));
+    }
+
+    @Test
+    public void whenPeramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst, is(new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("   ^")
+                .add("  ^^")
+                .add(" ^^^")
+                .add("^^^^")
+                .toString()));
+    }
+
+    @Test
+    public void whenPeramud() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst, is(new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("   ^   ")
+                .add("  ^^^  ")
+                .add(" ^^^^^ ")
+                .add("^^^^^^^")
+                .toString()));
+    }
+}
