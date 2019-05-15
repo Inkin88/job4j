35d93749977f8dd035e97a7ec7598876423d2fd8
Inkin88 <inkin_88@mail.ru>
2019-05-15 21:43:10 +0300

    checkstyle

--- chapter_004/src/main/java/ru/job4j/student/Student.java
+++ chapter_004/src/main/java/ru/job4j/student/Student.java
@@ -5,11 +5,11 @@ public class Student {
     private String name;
     private int scope;
 
-    public Student (String name, int scope) {
+    public Student(String name, int scope) {
         this.name = name;
         this.scope = scope;
     }
-    public Student () {
+    public Student() {
 
     }
     public String getName() {
