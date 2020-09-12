874b9e5b758dd7f5b67497a466f0de6c8338dda3
Inkin88 <inkin_88@mail.ru>
2020-09-12 14:30:49 +0300

    SimpleLinkedList

---
+++ chapter_005/src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,68 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleLinkedList<E> implements Iterable<E> {
+
+    private int size;
+    private int modCount;
+    Node<E> first;
+
+    private static class Node<E> {
+
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
+    }
+
+    public void add(E value) {
+        Node<E> newLink = new Node<E>(value);
+        newLink.next = first;
+        first = newLink;
+        size++;
+        modCount++;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> result = first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.data;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+
+        return new Iterator<E>() {
+            private final int expectedModCount = modCount;
+            private Node<E> node = first;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return node != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E data = node.data;
+                node = node.next;
+                return data;
+            }
+        };
+    }
+}
