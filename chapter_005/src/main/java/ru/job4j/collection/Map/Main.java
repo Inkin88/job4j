fd26345ee7706097702f85330b52f2e26e80e006
Inkin88 <inkin_88@mail.ru>
2020-10-05 16:12:45 +0300

    add Users to Map

---
+++ chapter_005/src/main/java/ru/job4j/collection/Map/Main.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection.Map;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("Airat", 2, new GregorianCalendar(1988, 4, 21));
+        User user2 = new User("Airat", 2, new GregorianCalendar(1988, 4, 21));
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, null);
+        map.put(user2, null);
+        System.out.println(map);
+
+    }
+}
