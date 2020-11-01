1aac5637cbb4b5f646ad09e23367719d87d19e90
Inkin88 <inkin_88@mail.ru>
2020-11-01 19:36:34 +0300

    Bot

--- chapter_006/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_006/src/main/java/ru/job4j/io/EchoServer.java
@@ -8,25 +8,33 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
     public static void main(String[] args) throws IOException {
-        boolean isWork = true;
         try (ServerSocket server = new ServerSocket(9000)) {
+            boolean isWork = true;
             while (isWork) {
                 Socket socket = server.accept();
-
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
+                    String msg = "Привет";
                     while (!(str = in.readLine()).isEmpty()) {
                         if (str.equals("GET /?msg=Bye HTTP/1.1")) {
                             isWork = false;
                         }
+                        if (str.startsWith("GET")) {
+                            String[] strings = str.split(" ");
+                            String[] strings1 = strings[1].split("=");
+                            msg = strings1[1];
+                        }
                         System.out.println(str);
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(msg.getBytes());
                 }
             }
         }
     }
 }
+
